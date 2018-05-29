9c73942dc3f7e3cf13a5bc9ebc203a62a15ac346
MBcdailey <chad.dailey@ministrybrands.com>
2018-05-29 18:01:06 -0400

    Created constants for all strings to save memory usage.

--- lib/element_builder/builder.rb
+++ lib/element_builder/builder.rb
@@ -1,7 +1,15 @@
 class Builder
 
+  DOCTYPE = "<!Doctype html>"
+  ELEMENT_FORMAT = "<%s%s>%s</%s>"
+  ELEMENT_FORMAT_SELF_CLOSING = "<%s%s>"
+  ATTRIBUTE_FORMAT = "%s=\"%s\""
+  ATTRIBUTE_FORMAT_BOOLEAN = "%s"
+  EMPTY_STRING = ""
+  SPACE = " "
+
   def build_html(arr, include_doctype = false)
-    html = include_doctype ? build_doctype : ""
+    html = include_doctype ? build_doctype : EMPTY_STRING
     arr.each { |i| html += build_element(i[:tag], i[:attrs], i[:content]) if hash_contains_tag?(i) } if arr.class == Array
     html
   end
@@ -9,22 +17,22 @@ class Builder
   private
 
   def build_doctype
-    "<!Doctype html>"
+    DOCTYPE
   end
   
   def element_format(tag)
-    %w(area base br col hr img input link meta param command keygen source).include?(tag) ? "<%s%s>" : "<%s%s>%s</%s>"
+    %w(area base br col hr img input link meta param command keygen source).include?(tag) ? ELEMENT_FORMAT_SELF_CLOSING : ELEMENT_FORMAT
   end
   
   def element_attribute_format(attr)
     %w(async autocomplete autofocus autoplay border challenge checked compact contenteditable controls default defer 
        disabled formNoValidate frameborder hidden indeterminate ismap loop multiple muted nohref noresize noshade 
        novalidate nowrap open readonly required reversed scoped scrolling seamless selected sortable spellcheck 
-       translate playsinline).include?(attr.to_s) ? "%s" : "%s=\"%s\""
+       translate playsinline).include?(attr.to_s) ? ATTRIBUTE_FORMAT_BOOLEAN : ATTRIBUTE_FORMAT
   end
   
   def build_attrs(attrs)
-    attrs.map { |k,v| format(element_attribute_format(k), k, v) }.join(" ")
+    attrs.map { |k,v| format(element_attribute_format(k), k, v) }.join(SPACE)
   end
   
   def handle_content(content)
@@ -33,7 +41,7 @@ class Builder
   end
 
   def build_element(tag, attrs, content)
-    format(element_format(tag), tag, attrs.class == Hash ? ' ' + build_attrs(attrs) : '', content.nil? ? '' : handle_content(content), tag)
+    format(element_format(tag), tag, attrs.class == Hash ? SPACE + build_attrs(attrs) : EMPTY_STRING, content.nil? ? EMPTY_STRING : handle_content(content), tag)
   end
   
   def hash_contains_tag?(hash)
